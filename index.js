d9897f2d1f2ec9d8f1425f752e38b8c819755d8b
Bart Langelaan <langelaanbart@gmail.com>
2016-10-12 02:05:48 +0200

    Add plugin

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const debug = require('debug')('sawmon:website-responsecode');
+
+module.exports.dependencies = ['sawmon-website-request'];
+
+module.exports.websites = {
+
+    schema: {
+        status: Number,
+        responseTime: Number
+    },
+
+    display: [
+        {
+            name: 'Response code',
+            value: website => (website.status ? `${website.status}` : null)
+        },
+        {
+            name: 'Response time',
+            value: website => (website.responseTime ? `${website.responseTime}s` : null)
+        }
+    ],
+
+    ping: passTrough => {
+
+        if (!passTrough.request || !passTrough.request.statusCode) {
+
+            debug('No statusCode found in passTrough.request');
+            debug(passTrough.request);
+
+            return Promise.resolve();
+
+        }
+        passTrough.instance.status = passTrough.request.statusCode;
+        passTrough.instance.responseTime = passTrough.request.elapsedTime / 1000;
+
+        return passTrough.instance.save();
+
+    }
+};
